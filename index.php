3463189e84799bda16516c9c04b8f5e903f41b3d
Alexey Demyanov <deal110988@him-mail.ch>
2025-05-19 15:01:50 +0200

    init

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+    Plugin name: Alex Word Filter Plugin
+    Description: Helps fltering out unwanted words in text
+    Version: 1.0
+    Author: Alex
+    Author URI: https://github.com/AlexeyDemyan
+*/
+
+if (! defined('ABSPATH')) exit; // Exit if accessed directly
